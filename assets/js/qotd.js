e7586d1a756a4ef74cd586874e720932c67bd9f9
Micah J Martin <mjm5097@rit.edu>
2017-02-20 02:15:14 -0500

    Added 'Quote of the Day' to the site

---
+++ assets/js/qotd.js
@@ -0,0 +1,15 @@
+function init() {
+	var current = new Date();
+	var first = new Date(current.getFullYear(), 0, 0);
+	var day_size = 1000 * 60 * 60 * 24;
+	var day = Math.floor((current - first) / day_size);
+	// get the number of days in a year and mod it by the number of quotes
+	document.getElementById('qotd_quote').innerHTML = "\""+quotes[day % 102][0]+"\"";
+	document.getElementById('qotd_author').innerHTML = "~ "+quotes[day % 102][1];
+	console.log("\""+quotes[day % 102][0]+"\"");
+	console.log("~ "+quotes[day % 102][1]);
+}
+
+window.onload = function() {
+	init();
+}
